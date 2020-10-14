1230c654195618504f0bf2111d2dc0b289ebf07f
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-14 14:05:36 +0530

    PINCode UC-1 validation

--- pinValidation.sh
+++ pinValidation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Validation Of PIN Code"
+
+read -p "enter pincode: " pincode
+
+#uc-1 only 6 digit pin code number
+pincode_Pattern="^[0-9]{6}$"
+if [[ $pincode =~ $pincode_Pattern ]]
+then
+        echo "Valid"
+else
+        echo "Invalid"
+fi
